7229328b0e855708dad4f8aa2a6295ce4d3f3d42
Igor <igor.carneiro13@hotmail.com>
2021-01-13 17:15:45 -0300

    Adicionando store, modal.

---
+++ src/components/howToUse/Modal.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="comp-modal">
+    <h2>Using Modals</h2>
+    
+    <button @click="showModal('first')">Show first modal</button>
+    <button @click="showModal('second')">Show second modal</button>
+    <button @click="showAllModals()">Show all modal</button>
+
+    <first />
+    <second />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+import First from '@/components/Modal/First.vue'
+import Second from '@/components/Modal/Second.vue'
+
+export default {
+  name: 'CompModal',
+  components: {
+    First,
+    Second
+  },
+  methods: {
+    ...mapActions({
+      showModal: 'global/showModal',
+      hideModal: 'global/hideModal',
+    }),
+    showAllModals() {
+      this.showModal('first')
+      this.showModal('second')
+    }
+  }
+}
+</script>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="default-layout">
     <comp-header />
+    <store />
+    <page-modal />
     <slot />
   </div>
 </template>
 
 <script>
 import CompHeader from '@/components/howToUse/Header.vue'
+import Store from '@/components/howToUse/Store.vue'
+import PageModal from '@/components/howToUse/Modal.vue'
 
 export default {
   name: 'DefaultLayout',
   components: {
-    CompHeader
+    CompHeader,
+    Store,
+    PageModal
   }
 }
 </script>
