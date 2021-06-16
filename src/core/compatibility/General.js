c9bdd04f46d3b49f1487740530c30f619ed80b40
Igor Carneiro <igor.carneiro13@hotmail.com>
2021-06-16 09:52:42 -0300

    Adicionando funçoes de scroll, desabilitar consoles e de identificaçao do sistema operacional do dispositivo em uso.

--- src/core/compatibility/General.js
+++ src/core/compatibility/General.js
@@ -15,6 +15,42 @@ export default {
     setPageTitle (title) {
       // Alterando título da página.
       document.title = title
+    },
+    // Redefinindo função console para não imprimir mensagens.
+    // listType pode ser ['log', 'info'].
+    disableConsoleFunction(listType) {
+      for (let type of listType) {
+        console[type] = () => {}
+      }
+    },
+    // Rola a página para um componente indicado pelo seu id ou classe, ou
+    // para valores de x e y.
+    scrollTo (id_or_class_or_x, y) {
+      if (y !== undefined) {
+        window.scrollTo(id_or_class_or_x, y)
+      } else {
+        document.querySelector(id_or_class_or_x).scrollIntoView({ 
+          behavior: 'smooth',
+          block: 'center'
+        });
+      }
+    },
+    // Identifica qual o sistema do dispositivo em uso.
+    identifySystemOperation() {
+      var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+      
+      let OS = 'unknown'
+
+      // Windows Phone must come first because its UA also contains "Android"
+      if (/windows phone/i.test(userAgent)) {
+        OS = 'Windows Phone'
+      } else if (/android/i.test(userAgent)) {
+        OS = 'Android'
+      } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) { // iOS detection
+        OS = 'iOS'
+      }
+
+      return OS
     }
   }
 }
